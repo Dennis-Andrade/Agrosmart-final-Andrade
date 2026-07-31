8b40a0908c4c74f2de37b428c24c45c4308d897a
Dennis Andrade <denisandradeg@gmail.com>
2026-07-30 20:24:40 -0500

    chore: configura perfil prod con postgresql y puerto propio

---
+++ src/main/java/ec/edu/espe/agrosmart/AgroSmartApplication.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.agrosmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroSmartApplication.class, args);
+    }
+}
